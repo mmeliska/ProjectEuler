7d30ddee54def0698b023b5cb09595645282ae4c
Michael Meliska <mike@meliska.net>
2016-05-01 08:52:45 -0700

    FSharp solution

---
+++ Problem1/Problem1.FSharp/Program.fs
@@ -0,0 +1,29 @@
+﻿
+
+let isMultiple divisor number =
+    number % divisor = 0
+
+
+let isMultipleOfDivisors divisors number = 
+    List.exists (fun elem -> isMultiple elem number) divisors
+
+
+let sumMultiples divisors exclusiveUpperBound =
+    let upperBound = exclusiveUpperBound - 1
+    [2..upperBound]
+        |> List.filter (fun x -> isMultipleOfDivisors divisors x)
+        |> List.sum
+    
+
+
+[<EntryPoint>]
+let main argv = 
+    let result = sumMultiples [3; 5;] 1000
+    System.Console.WriteLine(result)
+    System.Console.ReadLine()
+    0 // return an integer exit code
+
+
+
+
+
