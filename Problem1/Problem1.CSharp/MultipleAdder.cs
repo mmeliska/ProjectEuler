bd1cbf98e26b32b4b450aa74038f48dddbebf5f8
Michael Meliska <mike@meliska.net>
2016-05-01 07:43:39 -0700

    Problem 1 solution for C#

---
+++ Problem1/Problem1.CSharp/MultipleAdder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1.CSharp
+{
+    public class MultipleAdder
+    {
+        public int GetSumOfMultiples(List<int> divisors, int nonInclusiveUpperBound)
+        {
+            var multiples = FindMultiples(divisors, nonInclusiveUpperBound);
+            var result = multiples.Sum();
+
+            return result;
+        }
+
+        private List<int> FindMultiples(List<int> divisors, int nonInclusiveUpperBound)
+        {
+            var resultList = new List<int>();
+            for (int i = 2; i < nonInclusiveUpperBound; i++)
+            {
+                foreach (int d in divisors)
+                {
+                    if(i % d == 0)
+                    {
+                        resultList.Add(i);
+                        break;
+                    }
+                }
+            }
+
+            return resultList;
+        }
+    }
+}
---
+++ Problem1/Problem1.CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultipleAdder adder = new MultipleAdder();
+
+            var result = adder.GetSumOfMultiples(new List<int>() { 3, 5 }, 1000);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
