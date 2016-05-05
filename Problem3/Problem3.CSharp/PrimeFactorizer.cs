77ad27a97cc1d6b68ebfff8c23bd5e8d35eaf2f3
Michael Meliska <mike@meliska.net>
2016-05-05 14:37:38 -0700

    Csharp Solution

---
+++ Problem3/Problem3.CSharp/PrimeFactorizer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3.CSharp
+{
+    public class PrimeFactorizer
+    {
+        public List<long> PerformPrimeFactorization(long number)
+        {
+            long workingNumber = number;
+
+            List<long> primeFactors = new List<long>();
+
+            long? factor = null;
+
+            do
+            {
+                factor = FindFactor(workingNumber);
+                if(factor.HasValue)
+                {
+                    primeFactors.Add(factor.Value);
+                    workingNumber = workingNumber / factor.Value;
+                }
+            }
+            while (factor.HasValue);
+
+            if(workingNumber != number)
+                primeFactors.Add(workingNumber);
+
+            return primeFactors;
+        }
+
+
+        private long? FindFactor(long workingNumber)
+        {
+            for(int i = 2; i * i <= workingNumber; i++)
+            {
+                if (workingNumber % i == 0)
+                {
+                    return i;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Problem3/Problem3.CSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            long number = 600851475143;
+
+            var factors = new PrimeFactorizer().PerformPrimeFactorization(number);
+
+            if(factors.Any())
+                Console.WriteLine(factors.Max());
+            else
+                Console.Write("Number is prime");
+
+
+
+        }
+    }
+}
